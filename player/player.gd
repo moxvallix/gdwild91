a88297483d5b037571139740bbb2c84b48117ae6
Moxvallix Mox <moxvallix@gmail.com>
2026-03-22 16:02:43 +1030

    implement player up frames

--- player/player.gd
+++ player/player.gd
@@ -2,6 +2,7 @@ extends CharacterBody2D
 
 const SPEED = 600.0
 var followed: bool = false
+var dir := 2
 
 func _physics_process(delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
@@ -9,12 +10,6 @@ func _physics_process(delta: float) -> void:
 	var dir_x := Input.get_axis("move_left", "move_right")
 	var dir_y := Input.get_axis("move_up", "move_down")
 	
-	if dir_x > 0:
-		%Sprite.flip_h = false
-
-	if dir_x < 0:
-		%Sprite.flip_h = true
-
 	if dir_x:
 		velocity.x = dir_x * SPEED
 	else:
@@ -24,8 +19,37 @@ func _physics_process(delta: float) -> void:
 		velocity.y = dir_y * SPEED
 	else:
 		velocity.y = move_toward(velocity.y, 0, SPEED)
+	
+	if dir_y < 0:
+		dir = 0
+	elif dir_y > 0:
+		dir = 1
+	elif dir_x > 0:
+		dir = 2
+	elif dir_x < 0:
+		dir = 3
+
+	if dir_x or dir_y:
+		set_run_anim()
+	else:
+		set_idle_anim()
 
 	move_and_slide()
+
+func set_run_anim():
+	match dir:
+		0:
+			%Sprite.animation = &"run_up"
+			%Sprite.flip_h = false
+		2:
+			%Sprite.animation = &"run_side"
+			%Sprite.flip_h = false
+		3:
+			%Sprite.animation = &"run_side"
+			%Sprite.flip_h = true
+
+func set_idle_anim():
+	pass
 
 func reload():
 	get_tree().reload_current_scene()
